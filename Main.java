881c91d006809e89adf63a958ddaf53cce46c3b4
Lisanne Haase <lisannemariehaase@gmail.com>
2020-07-03 14:29:57 +0200

    Textdateien einlesen

--- Main.java
+++ Main.java
@@ -8,6 +8,12 @@ public class Main {
 
 		TuringMachine TM2 = ProgramsList.Anagram();
 		boolean done = TM2.Run("aab#aba");
+		
+		String As&Bs = "As&Bs.txt"; //Programm As&Bs wird eingelesen
+        ladeDatei(As&Bs);
+        
+        String Annagramm = "Annagramm.txt"; //Programm Annagramm wird eingelesen
+        ladeDatei(Annagramm);
 
 		if (done == true) {
 			System.out.println("The input was accepted.");
