622cbff178765a896d199bb4611fc56692efa7b8
Lisanne Haase <lisannemariehaase@gmail.com>
2020-07-09 15:13:32 +0200

    Fehlersuche

--- src/main/java/de/uni_hannover/hci/turing_machine/Main.java
+++ src/main/java/de/uni_hannover/hci/turing_machine/Main.java
@@ -12,18 +12,25 @@ public class ui extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+
     primaryStage.setTitle("Hello World!");
+    
     Button btn = new Button();
     btn.setText("Say 'Hello World'");
+    
     btn.setOnAction(new EventHandler<ActionEvent>() {
-    }
+    
+    
     @Override
     public void handle(ActionEvent event) {
     System.out.println("Hello World!");
     }
+    
     });
     StackPane root = new StackPane();
     root.getChildren().add(btn);
     primaryStage.setScene(new Scene(root, 300, 250));
     primaryStage.show();
+}   
 }
+
