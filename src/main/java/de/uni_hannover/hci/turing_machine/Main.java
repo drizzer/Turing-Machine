59e7e70fca5db00ea0a03628953fc603bc9b5585
Lisanne Haase <lisannemariehaase@gmail.com>
2020-07-09 14:52:51 +0200

    Verknüpfung Back-&Frontend

---
+++ src/main/java/de/uni_hannover/hci/turing_machine/Main.java
@@ -0,0 +1,29 @@
+package de.uni_hannover.hci.turing_machine;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    
+    @Override
+    public void start(Stage primaryStage) {
+    primaryStage.setTitle("Hello World!");
+    Button btn = new Button();
+    btn.setText("Say 'Hello World'");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
+    }
+    @Override
+    public void handle(ActionEvent event) {
+    System.out.println("Hello World!");
+    }
+    });
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+    primaryStage.setScene(new Scene(root, 300, 250));
+    primaryStage.show();
+}
--- src/main/java/de/uni_hannover/hci/turing_machine/components/ui/Controller.java
+++ src/main/java/de/uni_hannover/hci/turing_machine/components/ui/Controller.java
@@ -1,22 +1,43 @@
+
 package de.uni_hannover.hci.turing_machine.components.ui;
 
-import de.uni_hannover.hci.turing_machine.components.model.ProgramsList;
-
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
-public class Controller {
 
-
-    public class Main {
+    public class Controller {
 
         @FXML
         private Font x1;
 
         @FXML
         private Color x2;
+
+        @FXML
+        private Button loadBtn;
+
+        @FXML
+        private Button saveBtn;
+
+        @FXML
+        private Button startBtn;
+
+        @FXML
+        private Button stopBtn;
+
+        @FXML
+        private Button setAlphabet;
+
+        @FXML
+        private Button setState;
+
+        @FXML
+        private Button setMachineName;
 
         @FXML
         private TextField setName;
@@ -27,7 +48,157 @@ public class Controller {
         @FXML
         private Color x4;
 
+        @FXML
+        private Button load_btn;
+
+        @FXML
+        private Button save_btn;
+
+        @FXML
+        private TextField setalphabet_txt;
+
+        @FXML
+        private Button start_btn;
+
+        @FXML
+        private Button pause_btn;
+
+        @FXML
+        private Button stop_btn;
+
+        @FXML
+        private Button setalphabet_btn;
+
+        @FXML
+        private TableView<?> transition_txt;
+
+        @FXML
+        private Button setstate_btn;
+
+        @FXML
+        private Button setname_btn;
+
+        @FXML
+        private TextField setstate_txt;
+
+        @FXML
+        private TextField setname_txt;
+
+        @FXML
+        private ListView<?> action_txt;
+
+        @FXML
+        private TableView<?> outputtape_txt;
+
+        @FXML
+        private Button stepforward_btn;
+
+        @FXML
+        private Button stepback_btn;
+
+        @FXML
+        private TableView<?> CellidVisits_txt;
+
+        @FXML
+        private TableView<?> stepsStatechanges_txt;
+
+        @FXML
+        void load(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_CellidVisits_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_actions_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_outputtape_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_setalphabet_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_setname_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_setstate_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_stepsStatechange_txt(MouseEvent event) {
+
+        }
+
+        @FXML
+        void new_transition_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void pause(ActionEvent event) {
+
+        }
+
+        @FXML
+        void save(ActionEvent event) {
+
+        }
+
+        @FXML
+        void setalphabet(ActionEvent event) {
+
+        }
+
+        @FXML
+        void setname(ActionEvent event) {
+
+        }
+
+        @FXML
+        void setstate(ActionEvent event) {
+
+        }
+
+        @FXML
+        void start(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stepback(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stepforward(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stepsStatechange_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stop(ActionEvent event) {
+
+        }
+
     }
+
 
 
 
