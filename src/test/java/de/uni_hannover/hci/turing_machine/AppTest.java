e64c59f1dc1b6b81f7478c929b6c17b8f6403c13
Lisanne Haase <lisannemariehaase@gmail.com>
2020-07-09 11:04:22 +0200

    Änderung

--- src/test/java/de/uni_hannover/hci/turing_machine/AppTest.java
+++ src/test/java/de/uni_hannover/hci/turing_machine/AppTest.java
@@ -11,5 +11,6 @@ public class AppTest {
     @Test public void testAppHasAGreeting() {
         // App classUnderTest = new App();
         assertNotNull("app should have a greeting", "dummy");
+	//Testfälle prüfen, verschiedene Alphabete, Bandeingaben etc.
     }
 }
