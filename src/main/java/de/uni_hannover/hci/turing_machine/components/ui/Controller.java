46cb05c1adc75b88e3af5a7801a070355295587f
Lisanne Haase <lisannemariehaase@gmail.com>
2020-07-10 09:34:34 +0200

    Counter for statistic

--- src/main/java/de/uni_hannover/hci/turing_machine/components/ui/Controller.java
+++ src/main/java/de/uni_hannover/hci/turing_machine/components/ui/Controller.java
@@ -11,7 +11,202 @@ import javafx.scene.text.Font;
 
 
     public class Controller {
+        @FXML
+        private Font x1;
 
+        @FXML
+        private Color x2;
+
+        @FXML
+        private Button loadBtn;
+
+        @FXML
+        private Button saveBtn;
+
+        @FXML
+        private Button startBtn;
+
+        @FXML
+        private Button stopBtn;
+
+        @FXML
+        private Button setAlphabet;
+
+        @FXML
+        private Button setState;
+
+        @FXML
+        private Button setMachineName;
+
+        @FXML
+        private TextField setName;
+
+        @FXML
+        private Font x3;
+
+        @FXML
+        private Color x4;
+
+        @FXML
+        private Button load_btn;
+
+        @FXML
+        private Button save_btn;
+
+        @FXML
+        private TextField setalphabet_txt;
+
+        @FXML
+        private Button start_btn;
+
+        @FXML
+        private Button pause_btn;
+
+        @FXML
+        private Button stop_btn;
+
+        @FXML
+        private Button setalphabet_btn;
+
+        @FXML
+        private TableView<?> transition_txt;
+
+        @FXML
+        private Button setstate_btn;
+
+        @FXML
+        private Button setname_btn;
+
+        @FXML
+        private TextField setstate_txt;
+
+        @FXML
+        private TextField setname_txt;
+
+        @FXML
+        private ListView<?> action_txt;
+
+        @FXML
+        private TableView<?> outputtape_txt;
+
+        @FXML
+        private Button stepforward_btn;
+
+        @FXML
+        private Button stepback_btn;
+
+        @FXML
+        private TableView<?> CellidVisits_txt;
+
+        @FXML
+        private TableView<?> stepsStatechanges_txt;
+
+        @FXML
+        void load(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_CellidVisits_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_actions_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_outputtape_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_setalphabet_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_setname_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_setstate_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void new_stepsStatechange_txt(MouseEvent event) {
+
+        }
+
+        @FXML
+        void new_transition_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void pause(ActionEvent event) {
+
+        }
+
+        @FXML
+        void save(ActionEvent event) {
+
+        }
+
+        @FXML
+        void setalphabet(ActionEvent event) {
+
+        }
+
+        @FXML
+        void setname(ActionEvent event) {
+
+        }
+
+        @FXML
+        void setstate(ActionEvent event) {
+
+        }
+
+        @FXML
+        void start(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stepback(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stepforward(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stepsStatechange_txt(ActionEvent event) {
+
+        }
+
+        @FXML
+        void stop(ActionEvent event) {
+
+        }
+        /**
+        * getter und setter for JavaFX comonents
+        * @author Lisanne Haase
+         *@version 09.07.20
+        */
+        public Font getX1() {
+            return x1;
+        }
+
+        public void setX1(Font x1) {
+            this.x1 = x1;
+        }
         public Button getLoadBtn() {
             return loadBtn;
         }
@@ -244,201 +439,10 @@ import javafx.scene.text.Font;
             this.x2 = x2;
         }
 
-        @FXML
-        private Font x1;
-
-        @FXML
-        private Color x2;
-
-        @FXML
-        private Button loadBtn;
-
-        @FXML
-        private Button saveBtn;
-
-        @FXML
-        private Button startBtn;
-
-        @FXML
-        private Button stopBtn;
-
-        @FXML
-        private Button setAlphabet;
-
-        @FXML
-        private Button setState;
-
-        @FXML
-        private Button setMachineName;
-
-        @FXML
-        private TextField setName;
-
-        @FXML
-        private Font x3;
-
-        @FXML
-        private Color x4;
-
-        @FXML
-        private Button load_btn;
-
-        @FXML
-        private Button save_btn;
-
-        @FXML
-        private TextField setalphabet_txt;
-
-        @FXML
-        private Button start_btn;
-
-        @FXML
-        private Button pause_btn;
-
-        @FXML
-        private Button stop_btn;
-
-        @FXML
-        private Button setalphabet_btn;
-
-        @FXML
-        private TableView<?> transition_txt;
-
-        @FXML
-        private Button setstate_btn;
-
-        @FXML
-        private Button setname_btn;
-
-        @FXML
-        private TextField setstate_txt;
-
-        @FXML
-        private TextField setname_txt;
-
-        @FXML
-        private ListView<?> action_txt;
-
-        @FXML
-        private TableView<?> outputtape_txt;
-
-        @FXML
-        private Button stepforward_btn;
-
-        @FXML
-        private Button stepback_btn;
-
-        @FXML
-        private TableView<?> CellidVisits_txt;
-
-        @FXML
-        private TableView<?> stepsStatechanges_txt;
-
-        @FXML
-        void load(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_CellidVisits_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_actions_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_outputtape_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_setalphabet_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_setname_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_setstate_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void new_stepsStatechange_txt(MouseEvent event) {
-
-        }
-
-        @FXML
-        void new_transition_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void pause(ActionEvent event) {
-
-        }
-
-        @FXML
-        void save(ActionEvent event) {
-
-        }
-
-        @FXML
-        void setalphabet(ActionEvent event) {
-
-        }
-
-        @FXML
-        void setname(ActionEvent event) {
-
-        }
-
-        @FXML
-        void setstate(ActionEvent event) {
-
-        }
-
-        @FXML
-        void start(ActionEvent event) {
-
-        }
-
-        @FXML
-        void stepback(ActionEvent event) {
-
-        }
-
-        @FXML
-        void stepforward(ActionEvent event) {
-
-        }
-
-        @FXML
-        void stepsStatechange_txt(ActionEvent event) {
-
-        }
-
-        @FXML
-        void stop(ActionEvent event) {
-
-        }
-
-        public Font getX1() {
-            return x1;
-        }
-
-        public void setX1(Font x1) {
-            this.x1 = x1;
-        }
-    }
-
-
-
 
 }
+
+
+
+
+
