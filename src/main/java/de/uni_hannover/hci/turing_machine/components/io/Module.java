42b73a00a9b991508623bbb06e02c9e2c2ea9406
Mohamed El Hedi Atya <med_attia@hotmail.fr>
2020-07-15 21:36:37 +0200

    File module added

--- src/main/java/de/uni_hannover/hci/turing_machine/components/io/Module.java
+++ src/main/java/de/uni_hannover/hci/turing_machine/components/io/Module.java
@@ -1,52 +1,42 @@
-package prog2.io;
+package de.uni_hannover.hci.turing_machine.components.io;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException; 
+import java.io.PrintStream;
 
-class Module {
-/*
-  try {
-    // Creating an object of a file
-    File tape = new File("./io/tape.txt");
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
-    if (tape.createNewFile()) {
-    System.out.println("File created: " + tape.getName());
-    } else {
-    System.out.println("File already exists.");
+public class Module {
+  /*
+   * public static void main(String[] args) throws FileNotFoundException { 
+   * Module test = new Module();
+   * test.setFile("data1");
+   * Module.writeOutput("data2", "eyyyyyy"); 
+   * }
+   */
+
+  public void setFile(String fileName) throws FileNotFoundException {
+
+    try {
+      PrintStream console = new PrintStream("./" + fileName + ".txt");
+      System.setOut(console); // konsole inhalt in Objekt console speichern
+    } catch (FileNotFoundException fx) {
+      System.out.println(fx);
     }
-    } catch (IOException e) {
-    System.out.println("An error occurred.");
-    e.printStackTrace();
-    }
-    
+  }
 
-    public static void writeOutput() {
-      try {
-      FileWriter myWriter = new FileWriter("D:FileHandlingNewFilef1.txt");
-       // Writes this content into the specified file
-      myWriter.write(Java is the prominent programming language of the millenium!"); 
-
-      myWriter.close();
+  public static void writeOutput(String fileName, String newText) {
+    try {
+      FileWriter myWriter = new FileWriter(fileName + ".txt");
+      myWriter.write(newText);
       System.out.println("Successfully wrote to the file.");
-      } catch (IOException e) {
+      myWriter.close();
+
+    } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
-      }
+    }
 
-      public static void writeOutput() {
-        try {
-          FileWriter myWriter = new FileWriter("D:FileHandlingNewFilef1.txt");
-          
-          myWriter.write(Java is the prominent programming language of the millenium!"); 
-          
-          // Closing is necessary to retrieve the resources allocated
-          myWriter.close(); 
-          System.out.println("Successfully wrote to the file.");
-          } catch (IOException e) {
-          System.out.println("An error occurred.");
-          e.printStackTrace();
-          }
-      }
-*/
+  }
 }
