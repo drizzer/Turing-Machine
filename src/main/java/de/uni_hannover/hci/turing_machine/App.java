1af8bca293a4f89d00c83884c5a7c468b454b66b
Mohamed El Hedi Atya <med_attia@hotmail.fr>
2020-07-19 17:55:08 +0200

    invoked controller in App, declared stage in controller, open file

--- src/main/java/de/uni_hannover/hci/turing_machine/App.java
+++ src/main/java/de/uni_hannover/hci/turing_machine/App.java
@@ -1,24 +1,32 @@
 package de.uni_hannover.hci.turing_machine;
 
+import de.uni_hannover.hci.turing_machine.components.Controller;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-//import components.*;
-
 // VM options: --module-path %PATH_TO_FX%  --add-modules=javafx.controls,javafx.fxml
 
 public class App extends Application {
+    Controller controller = new Controller();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        de.uni_hannover.hci.turing_machine.components.Controller c = new de.uni_hannover.hci.turing_machine.components.Controller();
-        Parent root = FXMLLoader.load(getClass().getResource("/GUI.fxml"));
-        primaryStage.setTitle("Turing Machine");
-        primaryStage.setScene(new Scene(root, 1280, 800));
-        primaryStage.show();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI.fxml"));
+            Parent root = (Parent) loader.load();
+            Controller controller = (Controller) loader.getController(); // getting instance of the controller
+            controller.setPrimaryStage(primaryStage); // set stage
+            primaryStage.setTitle("Turing Machine");
+            primaryStage.setScene(new Scene(root, 1280, 800));
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/main/java/de/uni_hannover/hci/turing_machine/components/Controller.java
+++ src/main/java/de/uni_hannover/hci/turing_machine/components/Controller.java
@@ -7,25 +7,29 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.control.Tooltip;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
-import javafx.event.ActionEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import java.awt.event.ActionListener;
 
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
-import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 // import javafx.scene.control.cell.PropertyValueFactory;
@@ -35,73 +39,79 @@ import java.util.ResourceBundle;
 public class Controller extends TuringMachine implements ActionListener {
     // JavaFx components for the User Interface
 
-    // Constructor vom Typ Controller
-
-    Program TM = new Program();
-
-    // Objekt
-
+    private Stage primaryStage;
+    private Program TM = new Program(); // Object Typ Program
+    
     public Controller() {
     }
-
+    
+    // setter method for stage variable
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+    
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
         // TODO Auto-generated method stub
 
     }
-
-    // Object Typ TuringMachine
-
+    
+    @FXML
+    private HBox mainBox;
+    
+    @FXML
+    private AnchorPane config_pane;
+    
     @FXML
     private Font x1;
-
+    
     @FXML
     private Color x2;
-
+    
     @FXML
     private Button load_btn;
-
+    
     @FXML
     private Button save_btn;
-
+    
     @FXML
     private TextField setName_txt;
-
+    
     @FXML
     private Button setName_btn;
-
+    
     @FXML
     private TextField setAlphabet_txt;
-
+    
     @FXML
     private Button setAlphabet_btn;
-
+    
     @FXML
     private TextField setState_txt;
-
+    
     @FXML
     private Button setState_btn;
-
+    
     @FXML
     private TextField setTransition_txt;
-
+    
     @FXML
     private Button setTransition_btn;
 
     @FXML
     private Button editTransition_btn;
-
+    
     // Variable Output State Transitions
-
+    
     @FXML
     private TextField input_txt;
-
+    
     @FXML
     private Button stop_btn;
 
     @FXML
     private Button start_btn;
-
+    
     @FXML
     private Button pause_btn;
 
@@ -113,80 +123,52 @@ public class Controller extends TuringMachine implements ActionListener {
 
     @FXML
     private Button setstartState_btn;
-
+    
     @FXML
     private Button acceptState_btn;
-
+    
     @FXML
     private TextField acceptState_txt;
-
+    
     @FXML
     private TextField setstartState_txt;
-
+    
     @FXML
     private Font x3;
-
+    
     @FXML
     private Color x4;
 
     @FXML
     private TextArea transitionTable_txt;
-
+    
     @FXML
     private TextArea actionsList_txt;
-
+    
     @FXML
     private TextArea outputTape_txt;
-
+    
     @FXML
     private TextArea StateChanges_txt;
-
+    
     @FXML
     private TextArea steps_txt;
-
+    
     @FXML
     private TextArea CellidVisits_txt;
-
+    
     // Methods for input and buttons of GUI
     @FXML
     void acceptState(ActionEvent event) {
         setAcceptState(acceptState_txt.getText());
         acceptState_txt.setText(""); // emptys textfield
     }
-
+    
     @FXML
     void editTransiton(ActionEvent event) {
 
     }
-
-    @FXML
-    void load(ActionEvent event) {
-
-    }
-
-    @FXML
-    void pause(ActionEvent event) {
-
-    }
-
-    @FXML
-    void save(ActionEvent event) {
-        try {
-            File f = new File("./src/main/java/de/uni_hannover/hci/turing_machine/components/model/lib/saves/test.txt"); // New
-                                                                                                                         // file
-                                                                                                                         // is
-                                                                                                                         // created
-            boolean bool = false;
-            bool = f.createNewFile(); // checks if file is already there
-        } catch (Exception e) { // If file already exists
-            Tooltip tt = new Tooltip();
-            tt.setText("TM already exists.");
-            tt.setStyle("-fx-font: normal bold 12 Langdon; " + "-fx-base: #AE3522; " + "-fx-text-fill: orange;");
-            save_btn.setTooltip(tt);
-            save_btn.setText("Error");
-        }
-    }
-
+    
     @FXML
     void setAlphabet(ActionEvent event) {
         String temp = setAlphabet_txt.getText();
@@ -194,20 +176,20 @@ public class Controller extends TuringMachine implements ActionListener {
         TM.setnewAlphabet(setAlphabet_txt.getText());
         setTransition_txt.setText(temp); // empty text field
     }
-
+    
     @FXML
     void setName(ActionEvent event) {
         TM.setNameTM(setName_txt.getText());
         setName_txt.setText(""); // emptys textfield
         
     }
-
+    
     @FXML
     void setState(ActionEvent event) {
         TM.addState(setState_txt.getText());
         setState_txt.setText(""); // emptys the Textfield
     }
-
+    
     /**
      * This method sets the transitions in a transitionSet and prints the
      * transitions in a textfield below
@@ -219,15 +201,13 @@ public class Controller extends TuringMachine implements ActionListener {
      */
     @FXML
     void setTransiton(ActionEvent event) {
-
         // saves the input of transitions in Object TM
-
         String temp2 = setTransition_txt.getText();
-
+        
         transitionTable_txt.setText(temp2);
-
+        
         setTransition_txt.setText("");
-
+        
         String[] transition = temp2.split("; ");
 
         String rState = transition[0];
@@ -235,19 +215,18 @@ public class Controller extends TuringMachine implements ActionListener {
         String wState = transition[2];
         char wSymbol = temp2.charAt(12);
         boolean mDirection;
-
+        
         if (transition[5] == "R") {
             mDirection = true;
         } else {
             mDirection = false;
         }
-
+        
         TM.addTransition(rState, rSymbol, wState, wSymbol, mDirection);
-      
-        //Transitoins auflisten
-       // transitionTable_txt.setText(temp2);
+        // Transitoins auflisten
+        // transitionTable_txt.setText(temp2);
     }
-
+    
     @FXML
     void setstartState(ActionEvent event) {
         TM.setStartState(setstartState_txt.getText());
@@ -256,26 +235,57 @@ public class Controller extends TuringMachine implements ActionListener {
 
     @FXML
     void start(ActionEvent event) throws FileNotFoundException {
-
         start_btn.setText("Running..");
         start_btn.setStyle("-fx-font-size : 12");
 
         Tooltip tt = new Tooltip();
-
         tt.setText("Work is being done");
         tt.setStyle("-fx-font: normal bold 12 Langdon; " + "-fx-base: #AE3522; " + "-fx-text-fill: orange;");
-
         start_btn.setTooltip(tt);
-
+        
         TM.launch(input_txt.getText());
-
+        
         // Output state changes & steps statistics
-
         steps_txt.setText(Integer.toString(TM.statSteps));
-
         StateChanges_txt.setText(Integer.toString(TM.statChangeofstates));
-
         // Output Cell ID & Visits statistics ????
+    }
+    
+    @FXML
+    void pause(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void load(ActionEvent event) {
+        FileChooser file_chooser = new FileChooser();
+        EventHandler<ActionEvent> openEvent = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                // get the file selected
+                File file = file_chooser.showOpenDialog(primaryStage);
+
+                if (file != null) {
+                    actionsList_txt.setText(file.getAbsolutePath() + "  selected");
+                }
+                
+            }
+        };
+        load_btn.setOnAction(openEvent);
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+        try {
+            File f = new File("./src/main/java/de/uni_hannover/hci/turing_machine/components/model/lib/saves/test.txt");
+            boolean bool = false;
+            bool = f.createNewFile(); // checks if file is already there
+        } catch (Exception e) { // If file already exists
+            Tooltip tt = new Tooltip();
+            tt.setText("TM already exists.");
+            tt.setStyle("-fx-font: normal bold 12 Langdon; " + "-fx-base: #AE3522; " + "-fx-text-fill: orange;");
+            save_btn.setTooltip(tt);
+            save_btn.setText("Error");
+        }
     }
 
     @FXML
